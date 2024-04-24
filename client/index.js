a1e9f581154e4f40a9ac2bbf38bd275ad0fa81f4
d4n13lpro <cdap785@gmail.com>
2024-04-24 09:43:24 -0500

    se comenta el codigo

--- client/index.js
+++ client/index.js
@@ -1,139 +1,165 @@
+// Cuando el contenido del documento HTML ha sido completamente cargado y analizado, se ejecuta esta función
 document.addEventListener('DOMContentLoaded', function () {
-  fetch('http://localhost:5000/getAll')
-  .then(response => response.json())
-  .then(data => loadHTMLTable(data['data']));
+    // Se realiza una solicitud HTTP GET al servidor para obtener todos los datos de la tabla
+    fetch('http://localhost:5000/getAll')
+    .then(response => response.json()) // Se convierte la respuesta a JSON
+    .then(data => loadHTMLTable(data['data'])); // Se llama a la función loadHTMLTable para cargar los datos en la tabla HTML
+  });
   
-});
-
-document.querySelector('table tbody').addEventListener('click', function(event) {
-  if (event.target.className === "delete-row-btn") {
-      deleteRowById(event.target.dataset.id);
-  }
-  if (event.target.className === "edit-row-btn") {
-      handleEditRow(event.target.dataset.id);
-  }
-});
-
-const updateBtn = document.querySelector('#update-row-btn');
-const searchBtn = document.querySelector('#search-btn');
-
-searchBtn.onclick = function() {
-  const searchValue = document.querySelector('#search-input').value;
-
-  fetch('http://localhost:5000/search/' + searchValue)
-  .then(response => response.json())
-  .then(data => loadHTMLTable(data['data']));
-}
-
-function deleteRowById(id) {
-  fetch('http://localhost:5000/delete/' + id, {
-      method: 'DELETE'
-  })
-  .then(response => response.json())
-  .then(data => {
-      if (data.success) {
-          location.reload();
-      }
+  // Se agrega un evento 'click' a la tabla para manejar los clics en los botones de edición y eliminación de filas
+  document.querySelector('table tbody').addEventListener('click', function(event) {
+    if (event.target.className === "delete-row-btn") {
+        // Si se hace clic en el botón de eliminar, se llama a la función deleteRowById para eliminar la fila correspondiente
+        deleteRowById(event.target.dataset.id);
+    }
+    if (event.target.className === "edit-row-btn") {
+        // Si se hace clic en el botón de editar, se llama a la función handleEditRow para manejar la edición de la fila correspondiente
+        handleEditRow(event.target.dataset.id);
+    }
   });
-}
-
-function handleEditRow(id) {
-  const updateSection = document.querySelector('#update-row');
-  updateSection.hidden = false;
-  document.querySelector('#update-name-input').dataset.id = id;
-}
-
-updateBtn.onclick = function() {
-  const updateNameInput = document.querySelector('#update-name-input');
-
-
-  console.log(updateNameInput);
-
-  fetch('http://localhost:5000/update', {
-      method: 'PATCH',
-      headers: {
-          'Content-type' : 'application/json'
-      },
-      body: JSON.stringify({
-          id: updateNameInput.dataset.id,
-          name: updateNameInput.value
-      })
-  })
-  .then(response => response.json())
-  .then(data => {
-      if (data.success) {
-          location.reload();
-      }
-  })
-}
-
-
-const addBtn = document.querySelector('#add-name-btn');
-
-addBtn.onclick = function () {
-  const nameInput = document.querySelector('#name-input');
-  const name = nameInput.value;
-  nameInput.value = "";
-
-  fetch('http://localhost:5000/insert', {
-      headers: {
-          'Content-type': 'application/json'
-      },
-      method: 'POST',
-      body: JSON.stringify({ name : name})
-  })
-  .then(response => response.json())
-  .then(data => insertRowIntoTable(data['data']));
-}
-
-function insertRowIntoTable(data) {
-  console.log(data);
-  const table = document.querySelector('table tbody');
-  const isTableData = table.querySelector('.no-data');
-
-  let tableHtml = "<tr>";
-
-  for (var key in data) {
-      if (data.hasOwnProperty(key)) {
-          if (key === 'dateAdded') {
-              data[key] = new Date(data[key]).toLocaleString();
-          }
-          tableHtml += `<td>${data[key]}</td>`;
-      }
+  
+  // Seleccionar los botones de búsqueda y actualización
+  const updateBtn = document.querySelector('#update-row-btn');
+  const searchBtn = document.querySelector('#search-btn');
+  
+  // Evento 'click' para el botón de búsqueda
+  searchBtn.onclick = function() {
+    // Obtener el valor del campo de búsqueda
+    const searchValue = document.querySelector('#search-input').value;
+  
+    // Realizar una solicitud HTTP GET al servidor con el valor de búsqueda
+    fetch('http://localhost:5000/search/' + searchValue)
+    .then(response => response.json()) // Convertir la respuesta a JSON
+    .then(data => loadHTMLTable(data['data'])); // Llamar a la función loadHTMLTable para cargar los datos en la tabla HTML
   }
-
-  tableHtml += `<td><button class="delete-row-btn" data-id=${data.id}>Delete</td>`;
-  tableHtml += `<td><button class="edit-row-btn" data-id=${data.id}>Edit</td>`;
-
-  tableHtml += "</tr>";
-
-  if (isTableData) {
-      table.innerHTML = tableHtml;
-  } else {
-      const newRow = table.insertRow();
-      newRow.innerHTML = tableHtml;
+  
+  // Función para eliminar una fila por su ID
+  function deleteRowById(id) {
+    // Realizar una solicitud HTTP DELETE al servidor para eliminar la fila correspondiente
+    fetch('http://localhost:5000/delete/' + id, {
+        method: 'DELETE'
+    })
+    .then(response => response.json()) // Convertir la respuesta a JSON
+    .then(data => {
+        if (data.success) {
+            // Si la eliminación fue exitosa, recargar la página
+            location.reload();
+        }
+    });
   }
-}
-
-function loadHTMLTable(data) {
-  const table = document.querySelector('table tbody');
-
-  if (data.length === 0) {
-      table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
-      return;
+  
+  // Función para manejar la edición de una fila por su ID
+  function handleEditRow(id) {
+    // Mostrar la sección de actualización y asignar el ID a un campo oculto
+    const updateSection = document.querySelector('#update-row');
+    updateSection.hidden = false;
+    document.querySelector('#update-name-input').dataset.id = id;
   }
-
-  let tableHtml = "";
-
-  data.forEach(function ({id, name, date_added}) {
-      tableHtml += "<tr>";
-      tableHtml += `<td>${id}</td>`;
-      tableHtml += `<td>${name}</td>`;
-      tableHtml += `<td>${new Date(date_added).toLocaleString()}</td>`;
-      tableHtml += `<td><button class="delete-row-btn" data-id=${id}>Delete</td>`;
-      tableHtml += `<td><button class="edit-row-btn" data-id=${id}>Edit</td>`;
-      tableHtml += "</tr>";
-  });
-
-  table.innerHTML = tableHtml;
-}
+  
+  // Evento 'click' para el botón de actualización
+  updateBtn.onclick = function() {
+    // Obtener el valor del campo de actualización
+    const updateNameInput = document.querySelector('#update-name-input');
+  
+    // Realizar una solicitud HTTP PATCH al servidor para actualizar el nombre
+    fetch('http://localhost:5000/update', {
+        method: 'PATCH',
+        headers: {
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            id: updateNameInput.dataset.id,
+            name: updateNameInput.value
+        })
+    })
+    .then(response => response.json()) // Convertir la respuesta a JSON
+    .then(data => {
+        if (data.success) {
+            // Si la actualización fue exitosa, recargar la página
+            location.reload();
+        }
+    })
+  }
+  
+  // Evento 'click' para el botón de agregar nombre
+  const addBtn = document.querySelector('#add-name-btn');
+  
+  addBtn.onclick = function () {
+    // Obtener el valor del campo de entrada de nombre
+    const nameInput = document.querySelector('#name-input');
+    const name = nameInput.value;
+    nameInput.value = "";
+  
+    // Realizar una solicitud HTTP POST al servidor para agregar el nuevo nombre
+    fetch('http://localhost:5000/insert', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({ name : name})
+    })
+    .then(response => response.json()) // Convertir la respuesta a JSON
+    .then(data => insertRowIntoTable(data['data'])); // Llamar a la función insertRowIntoTable para insertar la nueva fila en la tabla HTML
+  }
+  
+  // Función para insertar una fila en la tabla HTML
+  function insertRowIntoTable(data) {
+    console.log(data);
+    const table = document.querySelector('table tbody');
+    const isTableData = table.querySelector('.no-data');
+  
+    let tableHtml = "<tr>";
+  
+    // Iterar sobre los datos y construir las celdas de la fila
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            if (key === 'dateAdded') {
+                // Formatear la fecha
+                data[key] = new Date(data[key]).toLocaleString();
+            }
+            tableHtml += `<td>${data[key]}</td>`;
+        }
+    }
+  
+    // Agregar botones de edición y eliminación a la fila
+    tableHtml += `<td><button class="delete-row-btn" data-id=${data.id}>Delete</td>`;
+    tableHtml += `<td><button class="edit-row-btn" data-id=${data.id}>Edit</td>`;
+  
+    tableHtml += "</tr>";
+  
+    // Insertar la fila en la tabla
+    if (isTableData) {
+        table.innerHTML = tableHtml;
+    } else {
+        const newRow = table.insertRow();
+        newRow.innerHTML = tableHtml;
+    }
+  }
+  
+  // Función para cargar los datos en la tabla HTML
+  function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
+  
+    // Si no hay datos, mostrar un mensaje
+    if (data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+        return;
+    }
+  
+    let tableHtml = "";
+  
+    // Iterar sobre los datos y construir las filas de la tabla
+    data.forEach(function ({id, name, date_added}) {
+        tableHtml += "<tr>";
+        tableHtml += `<td>${id}</td>`;
+        tableHtml += `<td>${name}</td>`;
+        tableHtml += `<td>${new Date(date_added).toLocaleString()}</td>`;
+        tableHtml += `<td><button class="delete-row-btn" data-id=${id}>Delete</td>`;
+        tableHtml += `<td><button class="edit-row-btn" data-id=${id}>Edit</td>`;
+        tableHtml += "</tr>";
+    });
+  
+    // Insertar las filas en la tabla
+    table.innerHTML = tableHtml;
+  }
+  
--- server/app.js
+++ server/app.js
@@ -1,72 +1,86 @@
+// Importar los módulos necesarios
 const express = require('express');
-const app = express();
-const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
+const app = express(); // Crear una instancia de la aplicación Express
+const cors = require('cors'); // Middleware para habilitar CORS (Cross-Origin Resource Sharing)
+const dotenv = require('dotenv'); // Para cargar variables de entorno desde un archivo .env
+dotenv.config(); // Configurar dotenv para cargar las variables de entorno
 
+// Importar el servicio de base de datos
 const dbService = require('./dbService');
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended : false }));
+// Configurar middlewares
+app.use(cors()); // Habilitar CORS
+app.use(express.json()); // Middleware para analizar solicitudes con formato JSON
+app.use(express.urlencoded({ extended : false })); // Middleware para analizar solicitudes con datos codificados en URL
 
-
-// create
+// Endpoint para crear un nuevo registro
 app.post('/insert', (request, response) => {
-    const { name } = request.body;
-    const db = dbService.getDbServiceInstance();
+    const { name } = request.body; // Obtener el nombre del cuerpo de la solicitud
+    const db = dbService.getDbServiceInstance(); // Obtener una instancia del servicio de base de datos
     
+    // Insertar el nuevo nombre en la base de datos
     const result = db.insertNewName(name);
 
+    // Manejar el resultado de la operación
     result
-    .then(data => response.json({ data: data}))
-    .catch(err => console.log(err));
+    .then(data => response.json({ data: data })) // Enviar los datos de vuelta como una respuesta JSON
+    .catch(err => console.log(err)); // Manejar cualquier error que ocurra
 });
 
-// read
+// Endpoint para obtener todos los registros
 app.get('/getAll', (request, response) => {
-    const db = dbService.getDbServiceInstance();
+    const db = dbService.getDbServiceInstance(); // Obtener una instancia del servicio de base de datos
 
+    // Obtener todos los datos de la base de datos
     const result = db.getAllData();
     
+    // Manejar el resultado de la operación
     result
-    .then(data => response.json({data : data}))
-    .catch(err => console.log(err));
-})
+    .then(data => response.json({ data: data })) // Enviar los datos de vuelta como una respuesta JSON
+    .catch(err => console.log(err)); // Manejar cualquier error que ocurra
+});
 
-// update
+// Endpoint para actualizar un registro existente
 app.patch('/update', (request, response) => {
-    const { id, name } = request.body;
-    const db = dbService.getDbServiceInstance();
+    const { id, name } = request.body; // Obtener el ID y el nuevo nombre del cuerpo de la solicitud
+    const db = dbService.getDbServiceInstance(); // Obtener una instancia del servicio de base de datos
 
+    // Actualizar el nombre en la base de datos por su ID
     const result = db.updateNameById(id, name);
     
+    // Manejar el resultado de la operación
     result
-    .then(data => response.json({success : data}))
-    .catch(err => console.log(err));
+    .then(data => response.json({ success: data })) // Enviar el resultado de vuelta como una respuesta JSON
+    .catch(err => console.log(err)); // Manejar cualquier error que ocurra
 });
 
-// delete
+// Endpoint para eliminar un registro por su ID
 app.delete('/delete/:id', (request, response) => {
-    const { id } = request.params;
-    const db = dbService.getDbServiceInstance();
+    const { id } = request.params; // Obtener el ID de los parámetros de la URL
+    const db = dbService.getDbServiceInstance(); // Obtener una instancia del servicio de base de datos
 
+    // Eliminar la fila de la base de datos por su ID
     const result = db.deleteRowById(id);
     
+    // Manejar el resultado de la operación
     result
-    .then(data => response.json({success : data}))
-    .catch(err => console.log(err));
+    .then(data => response.json({ success: data })) // Enviar el resultado de vuelta como una respuesta JSON
+    .catch(err => console.log(err)); // Manejar cualquier error que ocurra
 });
 
+// Endpoint para buscar registros por nombre
 app.get('/search/:name', (request, response) => {
-    const { name } = request.params;
-    const db = dbService.getDbServiceInstance();
+    const { name } = request.params; // Obtener el nombre de los parámetros de la URL
+    const db = dbService.getDbServiceInstance(); // Obtener una instancia del servicio de base de datos
 
+    // Buscar registros por nombre en la base de datos
     const result = db.searchByName(name);
     
+    // Manejar el resultado de la operación
     result
-    .then(data => response.json({data : data}))
-    .catch(err => console.log(err));
-})
+    .then(data => response.json({ data: data })) // Enviar los datos de vuelta como una respuesta JSON
+    .catch(err => console.log(err)); // Manejar cualquier error que ocurra
+});
 
+// Iniciar el servidor y escuchar en el puerto especificado en las variables de entorno
 app.listen(process.env.PORT, () => console.log('app is running'));
